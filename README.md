b88a2ad417a44d8ab0ac628bf539f08df18d68df
Valerio Santinelli <santinelli@altralogica.it>
2023-11-01 01:44:46 +0100

    Updated for the current master version of zig

---
+++ README.md
@@ -0,0 +1,9 @@
+# SDL compiled from source with Zig for macOS and iOS
+
+## macOS
+
+zig build -Dtarget=aarch64-macos
+
+## iOS
+
+zig build -Dtarget=aarch64-ios
--- build.zig
+++ build.zig
@@ -13,38 +13,36 @@ pub fn build(b: *Builder) void {
 
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const lib_cflags = &[_][]const u8{"-std=c99", "-ObjC"};
+    const lib_cflags = &[_][]const u8{ "-std=c99", "-ObjC", "-fobjc-weak" };
 
-    const sdl = b.addStaticLibrary("SDL2", null);
-    sdl.setBuildMode(mode);
-    sdl.setTarget(target);
+    const sdl = b.addStaticLibrary(.{ .name = "SDL2", .target = target, .optimize = optimize });
     sdl.linkSystemLibrary("c");
-    sdl.addIncludeDir("deps/sdl/include");
+    sdl.addIncludePath(.{ .path = "deps/sdl/include" });
     for (sdl_generic_src_files) |src_file| {
         const full_src_path = path.join(b.allocator, &[_][]const u8{ "deps", "sdl", src_file }) catch unreachable;
-        sdl.addCSourceFile(full_src_path, lib_cflags);
+        sdl.addCSourceFile(.{ .file = .{ .path = full_src_path }, .flags = lib_cflags });
     }
 
-    if (builtin.os.tag == .macos) {
-        //print("\n{s}", .{"macOS"});
+    if (target.getOsTag() == .macos) {
+        print("{s}\n", .{"Compiling for macOS"});
         // sdl.addIncludeDir("deps/sdl/src/hidapi");
         // sdl.addIncludeDir("deps/sdl/src/hidapi/hidapi");
         for (sdl_macos_src_files) |src_file| {
             const full_src_path = path.join(b.allocator, &[_][]const u8{ "deps", "sdl", src_file }) catch unreachable;
-            sdl.addCSourceFile(full_src_path, lib_cflags);
+            sdl.addCSourceFile(.{ .file = .{ .path = full_src_path }, .flags = lib_cflags });
         }
-        sdl.addFrameworkDir("/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks");
-        sdl.addSystemIncludeDir("/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include");
-        sdl.addSystemIncludeDir("/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include");
+        sdl.addFrameworkPath(.{ .path = "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks" });
+        sdl.addSystemIncludePath(.{ .path = "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include" });
+        sdl.addSystemIncludePath(.{ .path = "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include" });
         //sdl.addSystemIncludeDir("/usr/local/Cellar/llvm/10.0.1_1/lib/clang/10.0.1/include");
-        sdl.addSystemIncludeDir("/opt/homebrew/Cellar/llvm/13.0.1_1/lib/clang/13.0.1/include");
+        sdl.addSystemIncludePath(.{ .path = "/opt/homebrew/Cellar/llvm/13.0.1_1/lib/clang/13.0.1/include" });
         //sdl.addSystemIncludeDir("/usr/local/include");
-        sdl.addSystemIncludeDir("/opt/homebrew/include");
+        sdl.addSystemIncludePath(.{ .path = "/opt/homebrew/include" });
         //sdl.addSystemIncludeDir("/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/11.0.3/include");
-        sdl.addSystemIncludeDir("/usr/local/opt/llvm/include");
-        sdl.addSystemIncludeDir("/opt/homebrew/opt/llvm/include");
+        sdl.addSystemIncludePath(.{ .path = "/usr/local/opt/llvm/include" });
+        sdl.addSystemIncludePath(.{ .path = "/opt/homebrew/opt/llvm/include" });
 
         sdl.linkFramework("AppKit");
         sdl.linkFramework("AudioToolbox");
@@ -78,24 +76,62 @@ pub fn build(b: *Builder) void {
         // sdl.defineCMacro("SDL_VIDEO_OPENGL_ES2", "1");
         // sdl.defineCMacro("SDL_VIDEO_RENDER_OGL_ES2", "1");
     }
-    sdl.install();
 
-    const exe = b.addExecutable("test-zig-sdl", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+    if (target.getOsTag() == .ios) {
+        print("{s}\n", .{"Compiling for iOS"});
+        for (sdl_ios_src_files) |src_file| {
+            const full_src_path = path.join(b.allocator, &[_][]const u8{ "deps", "sdl", src_file }) catch unreachable;
+            sdl.addCSourceFile(.{ .file = .{ .path = full_src_path }, .flags = lib_cflags });
+        }
+        sdl.addFrameworkPath(.{ .path = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks" });
+        sdl.addSystemIncludePath(.{ .path = "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include" });
+        sdl.addSystemIncludePath(.{ .path = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/include" });
+        sdl.addSystemIncludePath(.{ .path = "/opt/homebrew/Cellar/llvm/13.0.1_1/lib/clang/13.0.1/include" });
+        sdl.addSystemIncludePath(.{ .path = "/opt/homebrew/include" });
+        sdl.addSystemIncludePath(.{ .path = "/opt/homebrew/opt/llvm/include" });
+
+        sdl.linkFramework("AudioToolbox");
+        sdl.linkFramework("AVFoundation");
+        sdl.linkFramework("CoreAudio");
+        sdl.linkFramework("CoreBluetooth");
+        sdl.linkFramework("CoreFoundation");
+        sdl.linkFramework("CoreGraphics");
+        sdl.linkFramework("CoreHaptics");
+        sdl.linkFramework("CoreMotion");
+        sdl.linkFramework("CoreServices");
+        sdl.linkFramework("CoreVideo");
+        sdl.linkFramework("GameController");
+        sdl.linkFramework("IOKit");
+        sdl.linkFramework("OpenGLES");
+        sdl.linkFramework("QuartzCore");
+        sdl.linkFramework("Security");
+        sdl.linkFramework("UIKit");
+
+        sdl.defineCMacro("TARGET_OS_IPHONE", "1");
+        sdl.defineCMacro("_THREAD_SAFE", "1");
+        sdl.defineCMacro("__APPLE__", "1");
+        sdl.defineCMacro("__IPHONEOS__", "1");
+    }
+
+    b.installArtifact(sdl);
+
+    const exe = b.addExecutable(.{ .name = "test-zig-sdl", .root_source_file = .{ .path = "src/main.zig" }, .target = target, .optimize = optimize });
     exe.linkLibrary(sdl);
-    exe.addIncludeDir("deps/sdl/include");
+    exe.addIncludePath(.{ .path = "deps/sdl/include" });
     //exe.addIncludeDir("deps/sdl/src/dynapi");
     //  export LDFLAGS="-L/usr/local/opt/llvm/lib"
-    exe.install();
+    if (target.getOsTag() == .ios) {
+        exe.addFrameworkPath(.{ .path = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks" });
+        exe.addSystemIncludePath(.{ .path = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/include" });
+        exe.linkSystemLibraryName("objc");
+    }
+    b.installArtifact(exe);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-
 }
 
 const sdl_generic_src_files = [_][]const u8{
@@ -196,7 +232,7 @@ const sdl_generic_src_files = [_][]const u8{
     "src/video/yuv2rgb/yuv_rgb.c",
 };
 
-const sdl_macos_src_files = [_][]const u8 {
+const sdl_macos_src_files = [_][]const u8{
     "src/audio/coreaudio/SDL_coreaudio.m",
     "src/audio/disk/SDL_diskaudio.c",
     "src/audio/dummy/SDL_dummyaudio.c",
@@ -252,6 +288,83 @@ const sdl_macos_src_files = [_][]const u8 {
     "src/video/cocoa/SDL_cocoashape.m",
     "src/video/cocoa/SDL_cocoavideo.m",
     "src/video/cocoa/SDL_cocoawindow.m",
+    "src/video/dummy/SDL_nullevents.c",
+    "src/video/dummy/SDL_nullframebuffer.c",
+    "src/video/dummy/SDL_nullvideo.c",
+};
+
+const sdl_ios_src_files = [_][]const u8{
+    "src/audio/coreaudio/SDL_coreaudio.m",
+    "src/audio/disk/SDL_diskaudio.c",
+    "src/audio/dummy/SDL_dummyaudio.c",
+    "src/file/cocoa/SDL_rwopsbundlesupport.m",
+    "src/filesystem/cocoa/SDL_sysfilesystem.m",
+    "src/haptic/darwin/SDL_syshaptic.c",
+    "src/hidapi/ios/hid.m",
+    "src/joystick/darwin/SDL_iokitjoystick.c",
+    "src/joystick/hidapi/SDL_hidapi_gamecube.c",
+    "src/joystick/hidapi/SDL_hidapi_luna.c",
+    "src/joystick/hidapi/SDL_hidapi_ps4.c",
+    "src/joystick/hidapi/SDL_hidapi_ps5.c",
+    "src/joystick/hidapi/SDL_hidapi_rumble.c",
+    "src/joystick/hidapi/SDL_hidapi_stadia.c",
+    "src/joystick/hidapi/SDL_hidapi_steam.c",
+    "src/joystick/hidapi/SDL_hidapi_switch.c",
+    "src/joystick/hidapi/SDL_hidapi_xbox360.c",
+    "src/joystick/hidapi/SDL_hidapi_xbox360w.c",
+    "src/joystick/hidapi/SDL_hidapi_xboxone.c",
+    "src/joystick/hidapi/SDL_hidapijoystick.c",
+    "src/joystick/iphoneos/SDL_mfijoystick.m",
+    "src/joystick/virtual/SDL_virtualjoystick.c",
+    "src/loadso/dlopen/SDL_sysloadso.c",
+    "src/locale/macosx/SDL_syslocale.m",
+    "src/misc/ios/SDL_sysurl.m",
+    "src/power/uikit/SDL_syspower.m",
+    "src/render/opengl/SDL_render_gl.c",
+    "src/render/opengl/SDL_shaders_gl.c",
+    "src/render/opengles/SDL_render_gles.c",
+    "src/render/opengles2/SDL_render_gles2.c",
+    "src/render/opengles2/SDL_shaders_gles2.c",
+    "src/render/software/SDL_blendfillrect.c",
+    "src/render/software/SDL_blendline.c",
+    "src/render/software/SDL_blendpoint.c",
+    "src/render/software/SDL_drawline.c",
+    "src/render/software/SDL_drawpoint.c",
+    "src/render/software/SDL_render_sw.c",
+    "src/render/software/SDL_rotate.c",
+    "src/render/software/SDL_triangle.c",
+    "src/sensor/coremotion/SDL_coremotionsensor.m",
+    "src/sensor/dummy/SDL_dummysensor.c",
+    "src/thread/pthread/SDL_syscond.c",
+    "src/thread/pthread/SDL_sysmutex.c",
+    "src/thread/pthread/SDL_syssem.c",
+    "src/thread/pthread/SDL_systhread.c",
+    "src/thread/pthread/SDL_systls.c",
+    "src/timer/unix/SDL_systimer.c",
+    "src/video/SDL_egl.c",
+    "src/video/cocoa/SDL_cocoaclipboard.m",
+    "src/video/cocoa/SDL_cocoaevents.m",
+    "src/video/cocoa/SDL_cocoakeyboard.m",
+    "src/video/cocoa/SDL_cocoamessagebox.m",
+    "src/video/cocoa/SDL_cocoamodes.m",
+    "src/video/cocoa/SDL_cocoamouse.m",
+    "src/video/cocoa/SDL_cocoaopengl.m",
+    "src/video/cocoa/SDL_cocoashape.m",
+    "src/video/cocoa/SDL_cocoavideo.m",
+    "src/video/cocoa/SDL_cocoawindow.m",
+    "src/video/uikit/SDL_uikitappdelegate.m",
+    "src/video/uikit/SDL_uikitclipboard.m",
+    "src/video/uikit/SDL_uikitevents.m",
+    "src/video/uikit/SDL_uikitmessagebox.m",
+    "src/video/uikit/SDL_uikitmetalview.m",
+    "src/video/uikit/SDL_uikitmodes.m",
+    "src/video/uikit/SDL_uikitopengles.m",
+    "src/video/uikit/SDL_uikitopenglview.m",
+    "src/video/uikit/SDL_uikitvideo.m",
+    "src/video/uikit/SDL_uikitview.m",
+    "src/video/uikit/SDL_uikitviewcontroller.m",
+    "src/video/uikit/SDL_uikitvulkan.m",
+    "src/video/uikit/SDL_uikitwindow.m",
     "src/video/dummy/SDL_nullevents.c",
     "src/video/dummy/SDL_nullframebuffer.c",
     "src/video/dummy/SDL_nullvideo.c",
